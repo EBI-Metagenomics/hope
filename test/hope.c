2b5d21833b0393267a9d1cfb5f3fd777255fa352
Danilo Horta <danilo.horta@pm.me>
2021-05-03 14:49:45 +0100

    tests passing

--- test/hope.c
+++ test/hope.c
@@ -7,11 +7,11 @@ int main()
     COND(3 == 3);
     NOTNULL(&a);
     ISNULL(NULL);
-    EQ("afdw", "afdwf");
+    EQ("afdw", "afdw");
     EQ(2, 2);
     COND(isinf(INFINITY));
     COND(isnan(NAN));
-    CLOSE(2.0, 1.0 + 1e-10);
+    CLOSE(1.0, 1.0 + 1e-10);
     CLOSE(0.0, 0.0);
     CLOSE2(0.0, 0.0 + 1e-10, 1e-9, 1e-9);
     return hope_status();
